3243dd6aa5031665f14229e3aaaf3f6fbc6541fc
sakshirautela <techsakshirautela@gmail.com>
2026-05-30 19:32:54 +0530

    routing updated

--- backend/src/main/java/com/junkbox/backend/controller/AddressController.java
+++ backend/src/main/java/com/junkbox/backend/controller/AddressController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/addresses")
+@RequestMapping("/api/addresses")
 @RequiredArgsConstructor
 public class AddressController {
 
